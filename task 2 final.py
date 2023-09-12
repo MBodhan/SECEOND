4c6ebad7b261402f8806f0534fb9fed3977e6864
MBodhan <144671484+MBodhan@users.noreply.github.com>
2023-09-12 16:22:35 +0530

    Add files via upload

---
+++ task 2 final.py
@@ -0,0 +1,52 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========================================================================= RESTART: C:/Users/pavannani/task 2.py ========================================================================
+
+===== Personal Budget Tracker =====
+1. Enter Income
+2. Enter Expense
+3. Calculate Budget
+4. Expense Analysis
+5. Exit
+Enter your choice: 10000
+Invalid choice. Please try again.
+
+===== Personal Budget Tracker =====
+1. Enter Income
+2. Enter Expense
+3. Calculate Budget
+4. Expense Analysis
+5. Exit
+Enter your choice: 1
+Enter your income: $1000
+Income of $1000.0 added successfully!
+
+===== Personal Budget Tracker =====
+1. Enter Income
+2. Enter Expense
+3. Calculate Budget
+4. Expense Analysis
+5. Exit
+Enter your choice: 2
+Enter expense category: SHOPPING
+Enter expense amount for SHOPPING: $200
+Expense of $200.0 for SHOPPING added successfully!
+
+===== Personal Budget Tracker =====
+1. Enter Income
+2. Enter Expense
+3. Calculate Budget
+4. Expense Analysis
+5. Exit
+Enter your choice: 3
+Total Income: $1000.0
+Total Expenses: $200.0
+Remaining Budget: $800.0
+
+===== Personal Budget Tracker =====
+1. Enter Income
+2. Enter Expense
+3. Calculate Budget
+4. Expense Analysis
+5. Exit
